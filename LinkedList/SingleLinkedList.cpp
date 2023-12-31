578f0b26f4cb2fb28215db3c88d2a83147d50d81
YSULTRA <yash22589@iiitd.ac.in>
2023-12-31 16:07:51 +0530

    LinkedList

---
+++ LinkedList/SingleLinkedList.cpp
@@ -0,0 +1,113 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+  int value;
+  Node *next;
+
+  Node(int value)
+  {
+    this->value = value;
+    this->next = NULL;
+  }
+};
+Node *insertAtHead(Node *&head, int value)
+{
+  Node *newNode = new Node(value);
+  newNode->next = head;
+  return newNode;
+}
+Node* insertAtPosition(Node* &head,int value,int index){
+  if(index == 0){
+    //case for head insertion
+    insertAtHead(head,value);
+  }else{
+    Node* temp = head;
+    int i=0;
+    while(i<index -1){
+      temp = temp -> next;
+      i++;
+    }
+    Node* new_node = new Node(value);
+    new_node->next = temp->next;
+    temp->next = new_node;
+    return head;
+  }
+}
+
+
+
+Node *insertAtEnd(Node *&head, int value)
+{
+  Node *temp = head;
+  Node *newNode = new Node(value);
+
+  if (temp == NULL)
+  {
+    head = newNode;
+  }
+  else
+  {
+    while (temp->next != NULL)
+    {
+      temp = temp->next;
+    }
+    temp->next = newNode;
+  }
+  return head;
+}
+
+void printLinkedList(Node *&head)
+{
+  Node *temp = head;
+  cout << "[ ";
+  while (temp != NULL)
+  {
+    cout << temp->value << " , ";
+    temp = temp->next;
+  }
+  cout << " ]";
+}
+
+void removeAtPosition(Node* &head,int position){
+  if(head ->next == NULL && position == 0){
+    delete head;
+  }else if(position == 0){
+    Node* temp = head;
+    head = head->next;
+    delete temp;
+  }else{
+    Node* temp=head;
+    int i=0;
+    while(i<position -1 && temp!=NULL){
+      temp=temp->next;
+      i++;
+    }
+    if(temp==NULL){
+      return ;
+    }else{
+      Node* dodelete=temp->next;
+      temp->next=dodelete->next;
+      delete dodelete;
+
+    }
+  }
+}
+
+int main()
+{
+
+  Node *head = new Node(1);
+  head = insertAtHead(head, 0);
+  head = insertAtEnd(head, 2);
+  head = insertAtEnd(head,4);
+  head = insertAtPosition(head,3,4);
+  head = insertAtPosition(head,5,5);
+
+  printLinkedList(head);
+  removeAtPosition(head,3);
+  cout<<endl;
+  printLinkedList(head);
+}
